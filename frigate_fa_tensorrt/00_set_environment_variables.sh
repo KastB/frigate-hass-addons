1d9c0397150b76f1bb29dc2feabf878370c140d0
Bernd Kast <kastbernd@gmx.de>
2024-02-16 19:13:21 +0100

    readme

--- frigate_fa_tensorrt/00_set_environment_variables.sh
+++ frigate_fa_tensorrt/00_set_environment_variables.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Check if options.json file exists
 if [ -f "/data/options.json" ]; then
     # Read environment variables from options.json
-    for var in $(jq -r '.env_vars | keys | .[]' /data/options.json); do
-        name=$(jq -r ".env_vars[$var].name" /data/options.json)
-        value=$(jq -r ".env_vars[$var].value" /data/options.json)
-        echo "Setting ${name} to ${value}"
-        export "${name}=${value}"
+    env_vars=$(jq -r '.env_vars' /data/options.json)
+    IFS=';' read -ra var_array <<< "$env_vars"
+    for variable in "${var_array[@]}"; do
+        # Split variable into name and value
+        IFS='=' read -r name value <<< "$variable"
+        # Write the environment variable to the container environment directory
+        echo "$value" > "/var/run/s6/container_environment/$name"
+        echo "Setting $name to $value globally"
     done
 else
     echo "Options file not found. No environment variables set."
 fi
-/init
--- frigate_fa_tensorrt/README.md
+++ frigate_fa_tensorrt/README.md
@@ -15,3 +15,32 @@ This version of the add-on requests full device access in order to turn off prot
 [aarch64-shield]: https://img.shields.io/badge/aarch64-yes-green.svg
 [amd64-shield]: https://img.shields.io/badge/amd64-yes-green.svg
 [armv7-shield]: https://img.shields.io/badge/armv7-yes-green.svg
+
+# NVIDIA RUNTIME
+
+You can't configure single addons to run with the nvidia-runtime. An option, however, is to configure the nvidia-runtime as the default, e.g.:
+```
+{
+    "log-driver": "journald",
+    "storage-driver": "overlay2",
+    "ip6tables": true,
+    "experimental": true,
+    "default-shm-size" : "128M",
+    "log-opts": {
+        "tag": "{{.Name}}"
+    },
+    "dns": [
+        "8.8.4.4",
+        "10.0.0.2",
+        "10.0.0.3"
+    ],
+    "runtimes": {
+        "nvidia": {
+            "args": [],
+            "path": "nvidia-container-runtime"
+        }
+    },
+   "default-runtime": "nvidia"
+}
+
+``
